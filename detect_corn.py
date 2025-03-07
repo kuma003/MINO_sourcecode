fix: handle single label case in detection and clarify occupancy calculation

detect_corn.py
@@ -79,16 +79,22 @@ class detector:
         nlabels, labels_img, stats, centroids = cv2.connectedComponentsWithStats(
             self.binarized_img.astype(np.uint8)
         )  # バウンディングボックス取得
+        if nlabels == 1:
+            self.is_detected = False
+            self.is_reached = False
+            return
         probabilities = np.abs(
             stats[:, cv2.CC_STAT_WIDTH] / stats[:, cv2.CC_STAT_HEIGHT] - self.cone_ratio
         )  # 値が0に近いほどコーンらしい形状 (>=0)
         self.is_detected = False
         idx_cone = -1  # コーンの要素番号
 
-        occupacies = stats[1:, cv2.CC_STAT_AREA] / imgSize  # 検知領域占有率
+        occupacies = (
+            stats[1:, cv2.CC_STAT_AREA] / imgSize
+        )  # 検知領域占有率 (背景(idx==1)は除く)
 
         idx_cone = np.argmax(occupacies) if np.max(occupacies) > 1 / 20000 else -1
-        idx_cone += 1  # 0番目は背景なので1を足す
+        idx_cone += 1  # 0番目は背景なので1から始める
 
         self.is_detected = np.max(occupacies) > (1 / 20000)
 